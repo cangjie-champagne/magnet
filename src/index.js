692c4730735f1f1a979b307d2649f24a31ce172f
João Pedro <joaopjt@icloud.com>
2023-09-14 23:42:55 -0300

    added y() method values

--- src/index.js
+++ src/index.js
@@ -1044,7 +1044,7 @@ class Magnet {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['moon'],
       web: []
     };
     
@@ -1060,19 +1060,19 @@ class Magnet {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['water'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['big'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
       }
     }
